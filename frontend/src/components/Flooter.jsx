76ac27d4743c7b2e4e437aa0b2fb89a2aa1e31d9
Kaveesha G.D.C <gdchamithkaveesha@gmail.com>
2025-03-08 19:53:41 +0530

    Refactor footer and navbar components; update root layout to include new components

--- frontend/src/components/Flooter.jsx
+++
@@ -1,8 +0,0 @@
-
-function Footer() {
-  return (
-    <div className="footer text-center bg-red-900 text-white p-3">
-      <p>© 2021 ByteSquad</p>
-    </div>
-  );
-}
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <div>
+        <h1 className=" bg-gradient-to-r from-black to-red-600 text-white p-3">Navbar</h1>
+        </div>
+    );
+    }
+
+export default Navbar;
